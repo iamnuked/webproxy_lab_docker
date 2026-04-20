18c119a2a6e58d117ccbfa41bcb80ea2ddeefba0
iamnuked <qjawls6262@gmail.com>
2026-04-20 22:45:16 +0900

    0420 코드 작성

--- webproxy-lab/echo/echoserver.c
+++ webproxy-lab/echo/echoserver.c
@@ -20,13 +20,30 @@ int main(int argc, char const *argv[]) {
     }
     
 
-    /* 코드 구현 부분: listen 소켓 생성, accept, echo 호출, close 흐름을 작성 */
-    Open_listenfd(argv[1]);
+    /* argv[1] 포트에서 클라이언트 접속을 기다리는 listen 소켓을 생성 */
+    listenfd = Open_listenfd(argv[1]);
+
+    /* 서버는 종료하지 않고 계속 다음 클라이언트 접속을 기다림 */
+    while(1) {
+        /* accept에 넘기기 전에 주소 구조체 크기를 알려줌 */
+        clientlen = sizeof(struct sockaddr_storage);
+
+        /* 클라이언트가 접속할 때까지 기다렸다가, 연결 전용 소켓 connfd를 반환 */
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+
+        /* accept가 채운 clientaddr을 사람이 읽을 수 있는 host/port 문자열로 변환 */
+        Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
+        /* 접속한 클라이언트 정보를 서버 터미널에 출력 */
+        printf("connected to (%s, %s)\n", client_hostname, client_port);
     
+        /* 연결된 클라이언트와 echo 통신을 수행 */
+        echo(connfd);
+        /* 클라이언트와의 연결이 끝났으므로 연결 소켓을 닫음 */
+        Close(connfd);
+    }
 
-
-
-    return 0;
+    /* while(1) 때문에 보통 여기까지 오지 않지만, 정상 종료 코드를 명시 */
+    exit(0);
 }
 
 void echo(int connfd) {
